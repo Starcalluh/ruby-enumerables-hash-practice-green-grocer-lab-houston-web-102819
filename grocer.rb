07a890177ce4399d6f6167730cffd1c7eb148221
George <starcalluh@live.com>
2019-10-07 20:33:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -6,7 +6,7 @@ def consolidate_cart(cart)
   element_name = element_hash.keys[0]
   element_stats = element_hash.values[0]
    
-  if final_hash.hash_key?[element_name] 
+  if final_hash.has_key?(element_name) 
     element_stats[:count] += 1 
     
   else
