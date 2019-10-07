88d9a676f8df5b33b5ec86800017f0823f4c4cc3
George <starcalluh@live.com>
2019-10-07 20:27:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -6,7 +6,7 @@ def consolidate_cart(cart)
   element_name = element_hash.keys[0]
   element_stats = element_hash.values[0]
    
-  if final_hash[element_name] 
+  if final_hash.hash_key?[element_name] 
      binding.pry
   else
     end
